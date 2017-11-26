b9bf1a32f83c597dd327a5549df2c0d2af4bb662
JeanCarloCorso <jeancarlocorso@hotmail.com>
2017-11-26 18:45:13 -0200

    Ajuste das views do cliente

--- resources/views/clientes/create.blade.php
+++ resources/views/clientes/create.blade.php
@@ -9,11 +9,6 @@
       <h1>
         Clientes
       </h1>
-      <ol class="breadcrumb">
-        <li><a href="home"><i class="fa fa-dashboard"></i> Home</a></li>
-        <li><a href="/clientes">Clientes</a></li>
-        <li class="active">Cadastro</li>
-      </ol>
     </section>
 @stop
 
