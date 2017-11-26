934a60c408de321fa1fdf625d37fa0c2af9d1d93
JeanCarloCorso <jeancarlocorso@hotmail.com>
2017-11-26 21:08:06 -0200

    Atualização do index de produtos

--- resources/views/produtos/index.blade.php
+++ resources/views/produtos/index.blade.php
@@ -29,7 +29,7 @@
           <th>Unidade Comprada</th>
           <th>Quantidade Comprada</th>
           <th>Nome Generico</th>
-          <th>Id Fornecedor</th>
+          <th>Fornecedor</th>
           <th>Editar</th>
           <th>Excluir</th>
         </tr>
